ef93ae7087688fed04c05d5c079511a6905cce89
A Ghorbani <ghorbani59@gmail.com>
2024-06-16 15:42:39 +0200

    Remove spaces in tempalates to make it visually easier to edit in applications

--- src/templates/danube2Template.ts
+++ src/templates/danube2Template.ts
@@ -3,27 +3,25 @@ import { TemplateConfig } from '../types';
 const danube2Template: TemplateConfig = {
   bosToken: '<s>',
   eosToken: '</s>',
-  chatTemplate: `
-        {%- if messages[0].role == 'system' -%}
-            {%- set system_message = messages[0].content -%}
-            {%- set messages = messages.slice(1) -%}
-        {%- else -%}
-            {%- set system_message = '' -%}
-        {%- endif %}
-
-        {{- system_message -}}
-
-        {%- for message in messages -%}
-            {%- if message.role == 'user' -%}
-                {{- '<|prompt|>' + message.content + eos_token -}}
-            {%- elif message.role == 'assistant' -%}
-                {{- '<|answer|>'  + message['content'] + eos_token -}}
-            {%- endif -%}
-            {%- if loop.last and add_generation_prompt -%}
-                {{- '<|answer|>' -}}
-            {%- endif -%}
-        {%- endfor -%}
-    `
+  // prettier-ignore
+  chatTemplate:
+    "{%- if messages[0].role == 'system' -%}" +
+    "    {%- set system_message = messages[0].content -%}" +
+    "    {%- set messages = messages.slice(1) -%}" +
+    "{%- else -%}" +
+    "    {%- set system_message = '' -%}" +
+    "{%- endif %}" +
+    "{{- system_message -}}" +
+    "{%- for message in messages -%}" +
+    "    {%- if message.role == 'user' -%}" +
+    "        {{- '<|prompt|>' + message.content + eos_token -}}" +
+    "    {%- elif message.role == 'assistant' -%}" +
+    "        {{- '<|answer|>'  + message['content'] + eos_token -}}" +
+    "    {%- endif -%}" +
+    "    {%- if loop.last and add_generation_prompt -%}" +
+    "        {{- '<|answer|>' -}}" +
+    "    {%- endif -%}" +
+    "{%- endfor -%}"
 };
 
 export default danube2Template;
--- src/templates/llama3Template.ts
+++ src/templates/llama3Template.ts
@@ -3,20 +3,19 @@ import { TemplateConfig } from '../types';
 const llama3Template: TemplateConfig = {
   bosToken: '<|begin_of_text|>',
   eosToken: '<|eot_id|>',
-  chatTemplate: `
-        {%- set loop_messages = messages -%}
-        {%- for message in loop_messages -%}
-            {%- set content = '<|start_header_id|>' + message.role + '<|end_header_id|>\n\n'+ message.content | trim + '<|eot_id|>' -%}
-            {%- if loop.index0 == 0 -%}
-                {%- set content = bos_token + content -%}
-            {%- endif -%}
-            {{- content -}}
-        {%- endfor -%}
-
-        {%- if add_generation_prompt -%}
-            {{- '<|start_header_id|>assistant<|end_header_id|>\n\n' -}}
-        {%- endif -%}
-    `
+  // prettier-ignore
+  chatTemplate:
+    "{%- set loop_messages = messages -%}" +
+    "{%- for message in loop_messages -%}" +
+    "    {%- set content = '<|start_header_id|>' + message.role + '<|end_header_id|>\n\n'+ message.content | trim + '<|eot_id|>' -%}" +
+    "    {%- if loop.index0 == 0 -%}" +
+    "        {%- set content = bos_token + content -%}" +
+    "    {%- endif -%}" +
+    "    {{- content -}}" +
+    "{%- endfor -%}" +
+    "{%- if add_generation_prompt -%}" +
+    "    {{- '<|start_header_id|>assistant<|end_header_id|>\n\n' -}}" +
+    "{%- endif -%}"
 };
 
 export default llama3Template;
